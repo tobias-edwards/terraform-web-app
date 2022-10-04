15765a4f23872a9ea0814275bd54b55f5cb3d453
tobias-edwards <tobias.edwards@publicissapient.com>
2022-10-04 12:48:02 +0100

    chore: initial commit

--- web-app/main.tf
+++ web-app/main.tf
@@ -1,3 +1,4 @@
+# Generate a random integer to create a (globally) unique name
 resource "random_string" "id" {
   length  = 4
   lower   = true
@@ -6,33 +7,44 @@ resource "random_string" "id" {
   upper   = false
 }
 
+# Resource Group - groups resources for easier management
 resource "azurerm_resource_group" "example" {
+  name     = "rg-example-${random_string.id.result}"
   location = "uksouth"
-  name     = "rg-web-app-${random_string.id.result}"
 }
 
+# App Service plan - defines the set of compute resources to run on (money, money, money)
 resource "azurerm_service_plan" "example" {
+  name                = "plan-example-${random_string.id.result}"
   location            = azurerm_resource_group.example.location
-  name                = "plan-web-app-${random_string.id.result}"
-  os_type             = "Linux"
   resource_group_name = azurerm_resource_group.example.name
-  sku_name            = "F1"
+  os_type             = "Linux"
+  sku_name            = "F1" # Free!
 }
 
+# App Service - Linux-based web app configuration
 resource "azurerm_linux_web_app" "example" {
+  name                = "app-example-${random_string.id.result}"
   location            = azurerm_resource_group.example.location
-  name                = "app-web-app-${random_string.id.result}"
   resource_group_name = azurerm_resource_group.example.name
   service_plan_id     = azurerm_service_plan.example.id
+  https_only          = true
+
   site_config {
-    always_on = false
+    always_on         = false
+    use_32_bit_worker = true
+
+    application_stack {
+      node_version = "16-lts"
+    }
   }
 }
 
+# Deployment centre - deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "example" {
   app_id                 = azurerm_linux_web_app.example.id
-  branch                 = "main"
-  repo_url               = "https://github.com/tobias-edwards/express-hello-world"
+  repo_url               = "https://github.com/tobias-edwards/nodejs-docs-hello-world"
+  branch                 = "master"
   use_manual_integration = true
   use_mercurial          = false
 }
